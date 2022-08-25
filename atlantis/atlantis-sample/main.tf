0cae9d80c28e003043c1ca2ce0e3a9bfb4c6df0a
kzk-maeda <kzk.maeda0711@gmail.com>
2022-08-25 15:16:54 +0900

    create resource

---
+++ atlantis/atlantis-sample/main.tf
@@ -0,0 +1,13 @@
+resource "aws_s3_bucket" "bucket" {
+  bucket = "terraform-ci-test-backet-atlantis-kzkmaeda"
+
+  tags = {
+    Name        = "terraform-ci-test-backet-atlantis-kzkmaeda"
+    Environment = "test"
+  }
+}
+
+resource "aws_s3_bucket_acl" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+  acl    = "private"
+}
