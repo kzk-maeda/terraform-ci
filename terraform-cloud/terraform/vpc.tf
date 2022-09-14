408ae4b0e9c72e0cbc5580aa89fdaa18bd70857f
kzk-maeda <kzk.maeda0711@gmail.com>
2022-09-14 23:20:14 +0900

    mod

--- terraform-cloud/terraform/vpc.tf
+++ terraform-cloud/terraform/vpc.tf
@@ -75,7 +75,7 @@ resource "aws_eip" "nat_1a" {
 }
 
 resource "aws_nat_gateway" "nat_1a" {
-  subnet_id     = aws_subnet.public_1c.id
+  subnet_id     = aws_subnet.public_1a.id
   allocation_id = aws_eip.nat_1a.id
 
   tags = {
