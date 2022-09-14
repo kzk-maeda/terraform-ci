f8554c811e236afdb78108e632eb8ffa44c2e949
kzk-maeda <kzk.maeda0711@gmail.com>
2022-09-14 09:39:39 +0900

    create vpc

--- terraform-cloud/terraform/backend.tf
+++ terraform-cloud/terraform/backend.tf
@@ -1,20 +1,9 @@
 terraform {
   cloud {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "kzk-maeda"
     workspaces {
       name = "terraform-cloud-sample"
     }
   }
 }
-
-provider "aws" {
-  region                   = "ap-northeast-1"
-  version                  = "~>3.0"
-  shared_credentials_file  = ".aws/credential"
-  profile                  = "ci"
-}
-
-data "aws_caller_identity" "current" {}
-
-data "aws_region" "current" {}
