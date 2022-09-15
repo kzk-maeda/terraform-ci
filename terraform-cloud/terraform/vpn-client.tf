b41e095edd1bbf37828e315afd3bfc831a2b4aa2
kzk-maeda <kzk.maeda0711@gmail.com>
2022-09-15 13:48:26 +0900

    change sg

--- terraform-cloud/terraform/vpn-client.tf
+++ terraform-cloud/terraform/vpn-client.tf
@@ -28,8 +28,7 @@ resource "aws_security_group" "this" {
     from_port        = 0
     to_port          = 0
     protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
+    cidr_blocks      = [aws_vpc.vpn_client.cidr_block]
   }
 
   egress {
